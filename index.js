92d1d894dbf6b38299347c4e7919b38af6ed5b3a
Adam DePaolo <amdepaolo@gmail.com>
2022-01-23 18:01:14 -0500

    commit tree 2015b309be2463ba8fe8b453deebf7b28e02bfa8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(giver, event){
+    const cardMessages = []
+    for (let a = 0; a < giver.length; a++) {
+        cardMessages.push(
+            `Thank you, ${giver[a]}, for the wonderful ${event} gift!`);
+    }
+    return cardMessages;
+}
+function countDown(number){
+    while (number> -1){
+        console.log(number); number--;
+        debugger;
+    }
+}
